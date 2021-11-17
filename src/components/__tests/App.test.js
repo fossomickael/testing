e34a62d44e424e0e8adb11f04f873978d2a47c13
Mickael FOSSO <fosso.mickael@gmail.com>
2021-11-17 09:56:55 +0100

    added absolute paths

--- src/components/__tests/App.test.js
+++ src/components/__tests/App.test.js
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "../App";
+import CommentBox from "components/CommentBox";
+import CommentList from "components/CommentBox";
+import { shallow } from "enzyme";
 
 it("shows a comment box", () => {
-  const div = document.createElement("div");
+  const wrapped = shallow(<App />);
 
-  ReactDOM.render(<App />, div);
-  expect(div.innerHTML).toContainHTML("CommentBox");
+  expect(wrapped.find(CommentBox).length).toEqual(1);
+});
 
-  ReactDOM.unmountComponentAtNode(div);
+it("shows a comment list", () => {
+  const wrapped = shallow(<App />);
+
+  expect(wrapped.find(CommentList).length).toEqual(1);
 });
--- src/components/setupTests.js
+++
@@ -1,4 +0,0 @@
-import Enzyme from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-
-Enzyme.configurre({ adapter: new Adapter() });
