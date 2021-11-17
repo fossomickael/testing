8194902274818f1ad647d34ce10809e6f8113dab
Mickael FOSSO <fosso.mickael@gmail.com>
2021-11-17 11:11:21 +0100

    submit comment test

--- src/components/__tests/CommentBox.test.js
+++ src/components/__tests/CommentBox.test.js
@@ -2,9 +2,41 @@ import React from "react";
 import { mount } from "enzyme";
 import CommentBox from "components/CommentBox";
 
-it("has a text area and a button", () => {
-  const wrapped = mount(<CommentBox />);
+let wrapped;
 
+beforeEach(() => {
+  wrapped = mount(<CommentBox />);
+});
+
+afterEach(() => {
+  wrapped.unmount();
+});
+
+it("has a text area and a button", () => {
   expect(wrapped.find("textarea").length).toEqual(1);
   expect(wrapped.find("button").length).toEqual(1);
+});
+
+it("has a text area that users can type in", () => {
+  wrapped.find("textarea").simulate("change", {
+    target: { value: "new comment" },
+  });
+
+  wrapped.update();
+
+  expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
+});
+
+it("empties the textarea field, when the form is submitted", () => {
+  wrapped.find("textarea").simulate("change", {
+    target: { value: "new comment" },
+  });
+
+  wrapped.update();
+
+  wrapped.find("form").simulate("submit");
+
+  wrapped.update();
+
+  expect(wrapped.find("textarea").prop("value")).toEqual("");
 });
